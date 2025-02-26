3e1b45589d503abdb1729d73b3b869e1ec867085
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2025-02-26 23:39:50 +0000

    ability to create tasks through the form

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -7,12 +7,27 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use \Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class TaskController extends AbstractController
 {
     #[Route('/tasks', methods:'GET')]
     public function show(EntityManagerInterface $entityManager): Response
     {
+        $tasks = $entityManager->getRepository(Task::class)->findAll();
+
+        return $this->render('tasks.html.twig', ['tasks' => $tasks]);
+    }
+
+    #[Route('/tasks', methods:'POST')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $task = new Task();
+        $task->setTitle($request->get('title'));
+
+        $entityManager->persist($task);
+        $entityManager->flush();
+
         $tasks = $entityManager->getRepository(Task::class)->findAll();
 
         return $this->render('tasks.html.twig', ['tasks' => $tasks]);
