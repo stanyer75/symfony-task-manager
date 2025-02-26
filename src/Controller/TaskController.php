46bac3ce616491e0517c3e0f05094d641c18c51f
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2025-02-26 22:51:04 +0000

    output all tasks

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
+use \Symfony\Component\HttpFoundation\Response;
 
 class TaskController extends AbstractController
 {
     #[Route('/tasks', methods:'GET')]
-    public function tasks()
+    public function show(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('tasks.html.twig');
+        $tasks = $entityManager->getRepository(Task::class)->findAll();
+
+        return $this->render('tasks.html.twig', ['tasks' => $tasks]);
     }
 }
